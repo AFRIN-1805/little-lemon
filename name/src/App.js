b4593358f5c91a674b5f8d33173a1642c06d96e6
AFRIN-1805echo <afrinbanu068@gmail.com>
2024-10-15 18:15:29 +0530

    i did

--- name/src/App.js
+++ name/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 function App() {
   return (
-    <div className="App">
-      Homepage
+    <><div>
+      clear all this
     </div>
+    <Header>
+      <img src='{logo192}' alt='little lemon logo'></img>
+      </Header>
+      <nav>
+      <a href='#'>hey taylor</a>
+      </nav>
+      </>
   );
 }
 export default App;
